89900870a0005994790a5e4c73a9ea33ad6b20d3
CodeVineDev <codevine2023@gmail.com>
2025-03-24 22:09:44 -0700

    Home styling complete

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-  fetch('./navfooter/navbar.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('navbar').innerHTML = data;
+const links = document.querySelectorAll('.navbar__links a');
+const currentPage = window.location.pathname.split("/").pop(); // this gets just the filename
 
-      // Now that the navbar is loaded, run active link check:
-      const links = document.querySelectorAll('.navbar__links a');
-      const currentPage = window.location.pathname.split("/").pop() || "index.html";
-      links.forEach(link => {
-        if (link.getAttribute('href') === currentPage) {
-          link.classList.add('active');
-        }
-      });
-    });
+links.forEach(link => {
+  if (link.getAttribute('href') === currentPage) {
+    link.classList.add('active');
+  }
+});
+
+const slider = document.getElementById('homeSlider');
+const sliderContent = slider.innerHTML;
+slider.innerHTML = `<div class="slider-track">${sliderContent + sliderContent}</div>`;
