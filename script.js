50da60e093e78bf3978023e6303c6cb9f9fdac0b
CodeVineDev <codevine2023@gmail.com>
2025-04-07 15:02:26 -0700

    preloader update v1

--- script.js
+++ script.js
@@ -7,48 +7,48 @@ e.forEach((e) => {
     AOS.init({ once: !0 });
     const e = document.querySelector(".slider-track");
     document.querySelectorAll(".slide").forEach((t) => {
-      let l = t.cloneNode(!0);
-      e.appendChild(l);
+      let n = t.cloneNode(!0);
+      e.appendChild(n);
     });
     const t = document.querySelector(".testimonial__slider-track");
     document.querySelectorAll(".testimonial__slide").forEach((e) => {
-      let l = e.cloneNode(!0);
-      t.appendChild(l);
+      let n = e.cloneNode(!0);
+      t.appendChild(n);
     });
   }),
-  gsap.registerPlugin(ScrollTrigger, CustomEase);
-document.querySelectorAll(".js-split").forEach((e) => {
-  ((e) => {
-    const t = e.innerText.split(" "),
-      l = t.map(
-        (e, l) =>
-          `<span class="word-wrapper"><span class="word">${
-            l === t.length - 1 ? e : `${e} `
-          }</span></span>`
-      );
-    e.innerHTML = l.join("");
-  })(e);
-  const t = e.querySelectorAll(".word");
-  gsap.set(t, { yPercent: 100, opacity: 0, filter: "blur(10px)" }),
-    gsap.to(t, {
-      scrollTrigger: {
-        trigger: e,
-        start: "10% 89%",
-        end: "10% 89%",
-        toggleActions: "play none none none",
-        once: !0,
-      },
-      yPercent: 0,
-      opacity: 1,
-      filter: "blur(0px)",
-      duration: 0.5,
-      ease: CustomEase.create(
-        "custom",
-        "M0,0 C0.402,0 0.238,0.39 0.428,0.7 0.574,0.938 0.818,1.001 1,1 "
-      ),
-      stagger: { amount: 0.1, ease: "power3.out" },
-    });
-}),
+  gsap.registerPlugin(ScrollTrigger, CustomEase),
+  document.querySelectorAll(".js-split").forEach((e) => {
+    ((e) => {
+      const t = e.innerText.split(" "),
+        n = t.map(
+          (e, n) =>
+            `<span class="word-wrapper"><span class="word">${
+              n === t.length - 1 ? e : `${e} `
+            }</span></span>`
+        );
+      e.innerHTML = n.join("");
+    })(e);
+    const t = e.querySelectorAll(".word");
+    gsap.set(t, { yPercent: 100, opacity: 0, filter: "blur(10px)" }),
+      gsap.to(t, {
+        scrollTrigger: {
+          trigger: e,
+          start: "10% 89%",
+          end: "10% 89%",
+          toggleActions: "play none none none",
+          once: !0,
+        },
+        yPercent: 0,
+        opacity: 1,
+        filter: "blur(0px)",
+        duration: 0.5,
+        ease: CustomEase.create(
+          "custom",
+          "M0,0 C0.402,0 0.238,0.39 0.428,0.7 0.574,0.938 0.818,1.001 1,1 "
+        ),
+        stagger: { amount: 0.1, ease: "power3.out" },
+      });
+  }),
   document.querySelectorAll(".accordion").forEach((e) => {
     e.addEventListener("click", function () {
       this.classList.toggle("active");
@@ -77,17 +77,20 @@ document.querySelectorAll(".js-split").forEach((e) => {
             ));
     });
   });
-const l = document.getElementById("hamburger"),
-  n = document.getElementById("mobileMenu"),
+const n = document.getElementById("hamburger"),
+  l = document.getElementById("mobileMenu"),
   o = document.querySelector(".equals"),
   s = document.querySelector(".times");
-l.addEventListener("click", () => {
-  n.classList.toggle("open");
-  const e = n.classList.contains("open");
+n.addEventListener("click", () => {
+  l.classList.toggle("open");
+  const e = l.classList.contains("open");
   (o.style.display = e ? "none" : "block"),
     (s.style.display = e ? "block" : "none");
-});
-window.addEventListener("load", function () {
-  const preloader = document.getElementById("preloader");
-  preloader.style.display = "none";
-});
+}),
+  window.addEventListener("load", () => {
+    const e = document.getElementById("preloader");
+    e.classList.add("fade-out"),
+      setTimeout(() => {
+        e.style.display = "none";
+      }, 700);
+  });
