427adad50026c7ca96e5de6d91fb7d2612905e03
CodeVineDev <codevine2023@gmail.com>
2025-04-07 13:48:38 -0700

    minified JS

--- script.js
+++ script.js
@@ -1,131 +1,89 @@
-const links = document.querySelectorAll(".navbar__links a");
-const currentPage = window.location.pathname.split("/").pop(); // this gets just the filename
-
-links.forEach((link) => {
-  if (link.getAttribute("href") === currentPage) {
-    link.classList.add("active");
-  }
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  AOS.init({
-    once: true, // Animations will only happen once
-  });
-
-  const sliderTrack = document.querySelector(".slider-track");
-  const slides = document.querySelectorAll(".slide");
-
-  // Duplicate slides for seamless effect
-  slides.forEach((slide) => {
-    let clone = slide.cloneNode(true);
-    sliderTrack.appendChild(clone);
-  });
-  const tstmSliderTrack = document.querySelector(".testimonial__slider-track");
-  const tstmSlides = document.querySelectorAll(".testimonial__slide");
-
-  // Duplicate slides for seamless effect
-  tstmSlides.forEach((slide) => {
-    let clone = slide.cloneNode(true);
-    tstmSliderTrack.appendChild(clone);
-  });
-});
-
-gsap.registerPlugin(ScrollTrigger, CustomEase);
-
-const CUSTOM_EASE =
-  "M0,0 C0.402,0 0.238,0.39 0.428,0.7 0.574,0.938 0.818,1.001 1,1 ";
-
-const splitIntoWords = (el) => {
-  const strArray = el.innerText.split(" ");
-  const words = strArray.map((word, i) => {
-    return `<span class="word-wrapper"><span class="word">${
-      i === strArray.length - 1 ? word : `${word} `
-    }</span></span>`;
-  });
-
-  el.innerHTML = words.join("");
-};
-
-const textElements = document.querySelectorAll(".js-split");
-
-textElements.forEach((el) => {
-  splitIntoWords(el);
-
-  const words = el.querySelectorAll(".word");
-
-  gsap.set(words, {
-    yPercent: 100,
-    opacity: 0,
-    filter: "blur(10px)",
-  });
-
-  gsap.to(words, {
-    scrollTrigger: {
-      trigger: el,
-      start: "10% 89%",
-      end: "10% 89%",
-      toggleActions: "play none none none",
-      once: true,
-    },
-    yPercent: 0,
-    opacity: 1,
-    filter: "blur(0px)",
-    duration: 0.5,
-    ease: CustomEase.create("custom", CUSTOM_EASE),
-    stagger: {
-      amount: 0.1,
-      ease: "power3.out",
-    },
-  });
-});
-
-document.querySelectorAll(".accordion").forEach((accordion) => {
-  accordion.addEventListener("click", function () {
-    // Toggle active class
-    this.classList.toggle("active");
-
-    // Get the next panel
-    const panel = this.nextElementSibling;
-
-    // Close other open panels
-    document.querySelectorAll(".panel").forEach((p) => {
-      if (p !== panel) {
-        p.style.maxHeight = null;
-        p.style.padding = "0 15px";
-        p.previousElementSibling.classList.remove("active");
-        p.previousElementSibling
-          .querySelector(".toggle-icon")
-          .setAttribute("icon", "ic:baseline-plus");
-      }
+const e = document.querySelectorAll(".navbar__links a"),
+  t = window.location.pathname.split("/").pop();
+e.forEach((e) => {
+  e.getAttribute("href") === t && e.classList.add("active");
+}),
+  document.addEventListener("DOMContentLoaded", function () {
+    AOS.init({ once: !0 });
+    const e = document.querySelector(".slider-track");
+    document.querySelectorAll(".slide").forEach((t) => {
+      let l = t.cloneNode(!0);
+      e.appendChild(l);
     });
-
-    // Toggle current panel
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-      panel.style.padding = "0 15px";
-      this.querySelector(".toggle-icon").setAttribute(
-        "icon",
-        "ic:baseline-plus"
+    const t = document.querySelector(".testimonial__slider-track");
+    document.querySelectorAll(".testimonial__slide").forEach((e) => {
+      let l = e.cloneNode(!0);
+      t.appendChild(l);
+    });
+  }),
+  gsap.registerPlugin(ScrollTrigger, CustomEase);
+document.querySelectorAll(".js-split").forEach((e) => {
+  ((e) => {
+    const t = e.innerText.split(" "),
+      l = t.map(
+        (e, l) =>
+          `<span class="word-wrapper"><span class="word">${
+            l === t.length - 1 ? e : `${e} `
+          }</span></span>`
       );
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-      panel.style.padding = "15px";
-      this.querySelector(".toggle-icon").setAttribute(
-        "icon",
-        "ic:baseline-minus"
-      );
-    }
+    e.innerHTML = l.join("");
+  })(e);
+  const t = e.querySelectorAll(".word");
+  gsap.set(t, { yPercent: 100, opacity: 0, filter: "blur(10px)" }),
+    gsap.to(t, {
+      scrollTrigger: {
+        trigger: e,
+        start: "10% 89%",
+        end: "10% 89%",
+        toggleActions: "play none none none",
+        once: !0,
+      },
+      yPercent: 0,
+      opacity: 1,
+      filter: "blur(0px)",
+      duration: 0.5,
+      ease: CustomEase.create(
+        "custom",
+        "M0,0 C0.402,0 0.238,0.39 0.428,0.7 0.574,0.938 0.818,1.001 1,1 "
+      ),
+      stagger: { amount: 0.1, ease: "power3.out" },
+    });
+}),
+  document.querySelectorAll(".accordion").forEach((e) => {
+    e.addEventListener("click", function () {
+      this.classList.toggle("active");
+      const e = this.nextElementSibling;
+      document.querySelectorAll(".panel").forEach((t) => {
+        t !== e &&
+          ((t.style.maxHeight = null),
+          (t.style.padding = "0 15px"),
+          t.previousElementSibling.classList.remove("active"),
+          t.previousElementSibling
+            .querySelector(".toggle-icon")
+            .setAttribute("icon", "ic:baseline-plus"));
+      }),
+        e.style.maxHeight
+          ? ((e.style.maxHeight = null),
+            (e.style.padding = "0 15px"),
+            this.querySelector(".toggle-icon").setAttribute(
+              "icon",
+              "ic:baseline-plus"
+            ))
+          : ((e.style.maxHeight = e.scrollHeight + "px"),
+            (e.style.padding = "15px"),
+            this.querySelector(".toggle-icon").setAttribute(
+              "icon",
+              "ic:baseline-minus"
+            ));
+    });
   });
-});
-
-const hamburger = document.getElementById("hamburger");
-const mobileMenu = document.getElementById("mobileMenu");
-const equalsIcon = document.querySelector(".equals");
-const timesIcon = document.querySelector(".times");
-
-hamburger.addEventListener("click", () => {
-  mobileMenu.classList.toggle("open");
-  const isOpen = mobileMenu.classList.contains("open");
-  equalsIcon.style.display = isOpen ? "none" : "block";
-  timesIcon.style.display = isOpen ? "block" : "none";
+const l = document.getElementById("hamburger"),
+  n = document.getElementById("mobileMenu"),
+  o = document.querySelector(".equals"),
+  s = document.querySelector(".times");
+l.addEventListener("click", () => {
+  n.classList.toggle("open");
+  const e = n.classList.contains("open");
+  (o.style.display = e ? "none" : "block"),
+    (s.style.display = e ? "block" : "none");
 });
